1f9b4e7d6bebd2a0ef7e87b57ed0f41e5e1b5f91
Dyscitygame <dyscitygame@gmail.com>
2022-05-14 09:07:50 -0700

    Added tests

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const https = require('https')
-const apiURL = "https://api.playerloop.io";
+const tiny = require('tiny-json-http')
+const axios = require('axios')
+const fs = require('fs')
+const apiURL = "http://localhost:1234";
 
 const playerloopSDK = function playerloopSDK(options = {}) {
   this.options = options
+  this.secret = options.secret
 }
 
 playerloopSDK.prototype.createReport  = function(options) {
   const self = this;
   self.options = { ...self.options, ...options }
-  if (!self.options.type) {
+  if (!self.options.type || self.options.type == "") {
     self.options.type = "bug"
   }
   if (self.options.type != "bug" && self.options.type != "feedback") {
@@ -23,34 +26,99 @@ playerloopSDK.prototype.createReport  = function(options) {
   if (!self.options.secret) {
     throw new Error("Secret is mandatory")
   }
-  self.options.client = "npm"
-  const call = new Promise((resolve, reject) => {
-    https.request({
-      host: apiURL,
-      port: 443,
-      body: options,
-      method: "POST",
-      path: "/reports",
-      headers: {
-        "Authorization": self.options.secret,
-        "Content-Type": "application/json"
-      }
-    }, function(res) {
-      if (res.statusCode > 201) {
-        reject(res.statusMessage)
-      }
-      resolve(res.body)
+  if (!self.options.player) {
+    self.options.player = {"id":"dsfffdfsd"}
+  }
+  self.options.client = "javascript"
+  delete self.options.secret
+  if (self.options.attachments) {
+    var attachments = self.options.attachments
+    delete self.options.attachments
+    const prom = new Promise((resolve, reject) => {
+      const call = tiny.post({
+        url: apiURL + "/reports",
+        data: self.options,
+        headers: {
+          "Authorization": self.secret,
+          "Content-Type": "application/json"
+        }
+      })
+      call
+      .then(function(data) {
+        const reportId = data.body.data.id
+        var uploadedA = 0
+        //console.log(data.body.data.id)
+        attachments.forEach(function(att) {
+          self.uploadAttachment(att, reportId)
+          .then(function(data) {
+            uploadedA++
+            if (uploadedA >= attachments.length) {
+              resolve(reportId);
+            }
+          })
+          .catch(function(err) {
+            reject(err)
+          })
+        })
+      })
+      .catch(function(err) {
+        console.log("ERR "+err)
+        reject(err)
+      })
+    });
+    return prom
+
+  } else {
+    const prom = new Promise((resolve, reject) => {
+      const call = tiny.post({
+          url: apiURL + "/reports",
+          data: self.options,
+          headers: {
+            "Authorization": self.secret,
+            "Content-Type": "application/json"
+          }
+        })
+        call
+        .then(function(data) {
+          resolve(data.body.data.id)
+        })
+        .catch(function(err) {
+          reject(err.body)
+        })
     })
-  })
-  return call;
+    return prom;
+  }
+}
+
+playerloopSDK.prototype.uploadAttachment  = function(filepath, reportId) {
+  const self = this;
+  const promise = new Promise((resolve, reject) => {
+    var url = apiURL + "/reports/"+reportId+"/attachment"
+    axios.post(url, {
+      //id: filepath,
+      file: fs.createReadStream(filepath)
+    }, {
+      headers: {
+        "Authorization": self.secret,
+        'Content-Type': 'multipart/form-data',
+        "Content-Disposition": "form-data",
+        "filename" : "bella.js",
+        "name" : "file"
+      }
+    })
+    .then(function(data) {
+      resolve(reportId)
+    })
+    .catch(function(err) {
+      reject(err)
+    })
+
+  });
+  return promise;
 }
 
 playerloopSDK.prototype.createPlayer  = function() {
   /* missing */
-}
-
-function validatePlayerObject() {
-  /* here we should validate the optional player object on the client side if needed */
 }
 
 module.exports = playerloopSDK;
