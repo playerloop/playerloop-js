2700b52a885e8b8cbcbb1141daa7c30b7c72c07a
effe-m <francesco.megna97@gmail.com>
2022-10-26 21:52:49 +0200

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-this-alias': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
     },
     settings: {
         react: {
